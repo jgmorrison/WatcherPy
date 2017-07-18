2475dbca2a2fc19da4058d215edde91935ca6fd3
jgmorrison <jmorrison@continuum.io>
2017-07-18 13:46:22 -0500

    Updating setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from distutils.core import setup
 setup(
     name ='watcherpy',
     version ='0.1.0',
-    description ='Simple CLI tool to watch and eload html files on change.',
+    description ='Simple CLI tool to watch html and css files and reload the browser on change.',
     author ='John Morrison',
     author_email ='john@johmorrison.io',
+    url = "https://github.com/jgmorrison/WatcherPy",
     packages = ["watcherpy"],
     entry_points = {"console_scripts" : ["watcher = watcherpy.__main__:main"]}
 )
