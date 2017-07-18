14625e246dfef9d1aa15c7801fdb3848fff0e738
jgmorrison <jmorrison@continuum.io>
2017-07-18 16:25:33 -0500

    Update to 0.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README'), encoding='utf-8') as f:
 
 setup(
     name ='eyeon',
-    version ='0.1.1',
+    version ='0.1.2',
     description ='Simple CLI tool to watch html and css files and reload the browser on change.',
     long_description = long_description,
     author ='John Morrison',
