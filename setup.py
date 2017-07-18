c8bcf3962c0a8d0a0e8ec777cf3a16b95c8136c4
jgmorrison <jmorrison@continuum.io>
2017-07-18 14:29:19 -0500

    Added setup.cfg

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     name ='watcherpy',
     version ='0.1.0',
     description ='Simple CLI tool to watch html and css files and reload the browser on change.',
+    long_description = open("README.rst").read(),
     author ='John Morrison',
     author_email ='john@johmorrison.io',
-    url = "https://github.com/jgmorrison/WatcherPy",
+    url = "https://github.com/jgmorrison/watcherpy",
     packages = ["watcherpy"],
     entry_points = {"console_scripts" : ["watcher = watcherpy.__main__:main"]}
 )
